29215e4b606d2ee673e57aeea10f4068c0d41787
lbq <lbq@qq.com>
2016-06-22 18:12:36 +0800

    更改了控制器

--- dd/dd/ViewController.m
+++ dd/dd/ViewController.m
@@ -16,12 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+   //点附近的开始减肥的
 }
 
 @end
